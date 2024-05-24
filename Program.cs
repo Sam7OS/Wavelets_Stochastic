a1f7a500e243ca7f8172d291a62caa3ebba4cd3b
Sam7OS <samuel.ouarbi@gmail.com>
2024-05-24 09:14:14 +0200

    Test des ondelettes sur une série de nombres

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using WaveLibrary;
+using ArrayCalc;
+
+int Min = 100;
+int Max = 1000;
+Random randNum = new();
+double[] tab_d = Enumerable.Repeat(0, 256).Select(i => Min+(Max-Min)*randNum.NextDouble()).ToArray();
+
+Vector vec1 = new(tab_d);
+
+int pct_smooth = 50;
+
+Daub_Waves daub = new(vec1,pct_smooth);
+
+Haar_Waves haar = new(vec1,pct_smooth);
+
+Console.WriteLine("Process done");
